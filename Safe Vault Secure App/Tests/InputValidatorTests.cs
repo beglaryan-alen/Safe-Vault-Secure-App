7a465a742e87af751c107111c105fc5dacb0e0ac
alen.beglaryan.2017 <alen.beglaryan.2017@gmail.com>
2025-04-05 19:36:32 +0400

    added tests

---
+++ Safe Vault Secure App/Tests/InputValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Safe_Vault_Secure_App.Validators;
+using Xunit;
+
+namespace Safe_Vault_Secure_App.Tests
+{
+    public class InputValidatorTests
+    {
+        [Fact]
+        public void ValidUsername_ReturnsTrue()
+        {
+            Assert.True(InputValidator.IsValidUsername("john_doe"));
+        }
+
+        [Fact]
+        public void InvalidUsername_ReturnsFalse()
+        {
+            Assert.False(InputValidator.IsValidUsername("!!!bad$$$"));
+        }
+
+        [Fact]
+        public void ValidEmail_ReturnsTrue()
+        {
+            Assert.True(InputValidator.IsValidEmail("user@example.com"));
+        }
+
+        [Fact]
+        public void InvalidEmail_ReturnsFalse()
+        {
+            Assert.False(InputValidator.IsValidEmail("user@@example..com"));
+        }
+    }
+}
